fe8528f10e84b2a442560a1afc0544c268ead4ae
Licul Mauro <liculm89@gmail.com>
2016-07-20 19:38:33 +0200

    Renamed variable names to follow PEP8 ...still in progress

--- Scripts/mot_red_plt.py
+++ Scripts/mot_red_plt.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#! /usr/bin/python3
+#coding: utf-8
 """
 Created on Sat Mar 26 13:33:44 2016
 
@@ -12,31 +13,31 @@ Motor: Dunkermotor BG 44 SI
 """
 import numpy as np
 
-N_nom = 2860  #rpm
-T_nom = 5.7/100. #Nm 
-N_min = 100 #rpm
+N_NOM = 2860  #rpm
+T_NOM = 5.7/100.0 #
+N_MIN = 100 #rpm
 
 T_const = 4.8/100. #Nm/A
 
 Eta_reduktor = 0.8
 #i_r = np.array()
-i_arr = [14.,19.,25., 27., 46.,71.]
-i_red = [14.,19.,25., 27., 46.,71.]
-i_reduktor = 25
-i_kosara = 3.2
-i_total = i_reduktor * i_kosara
-for i in range(len(i_arr)):
-    i_arr[i] = i_arr[i]*i_kosara
-N_kos_Nom = N_nom / i_total
-T_kos_Nom = T_nom * i_total * Eta_reduktor
+I_ARR = [14., 19., 25., 27., 46., 71.]
+I_RED = [14., 19., 25., 27., 46., 71.]
+I_REDUKTOR = 25
+I_KOSARA = 3.2
+I_TOTAL = I_REDUKTOR * I_KOSARA
+for i in range(len(I_ARR)):
+    I_ARR[i] = I_ARR[i]*I_KOSARA
+N_KOS_Nom = N_NOM / I_TOTAL
+T_kos_Nom = T_NOM * I_TOTAL * Eta_reduktor
 
-N_kos = np.linspace(0, 60 , 121)
+N_KOS = np.linspace(0, 60 , 121)
 N_motor_arr = []
-for i in i_arr:
-    N_motor_arr.append(N_kos*i)
+for i in I_ARR:
+    N_motor_arr.append(N_KOS*i)
  
-N_motor = N_kos * i_total
-N_m_1 = N_kos * i_arr[1]
+N_motor = N_KOS * I_TOTAL
+N_m_1 = N_KOS * I_ARR[1]
 import matplotlib
 import matplotlib.pylab as plt
 #from matplotlib import rc
@@ -47,9 +48,9 @@ plt.rc('font', family='serif')
 
 for item,num in enumerate(N_motor_arr):
     #print (num)
-    plt.plot(N_kos, N_motor_arr[item], label='$i = %i*3.2$' % i_red[item])
+    plt.plot(N_KOS, N_motor_arr[item], label='$i = %i*3.2$' % I_RED[item])
 #for i in range(len(N_motor_arr)):
-#    plt.plot(N_kos, N_motor_arr[i], label='$i = %i*3.2$' % i_red[i])
+#    plt.plot(N_KOS, N_motor_arr[i], label='$i = %i*3.2$' % I_RED[i])
 
 plt.ylim(ymax = 7000, ymin = 0)
 plt.minorticks_on()
