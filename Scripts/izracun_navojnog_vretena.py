c3af38428e9186a8591a08f3b5bfb8d4a1ff5253
Mauro Licul <liculm89@gmail.com>
2016-07-24 13:55:01 +0200

    formated scripts using autopep8

--- Scripts/izracun_navojnog_vretena.py
+++ Scripts/izracun_navojnog_vretena.py
@@ -9,54 +9,54 @@ Created on Thu Jul 14 14:01:29 2016
 
 import numpy as np
 
-#Dimenzije vretena
-#Tip B+B+K Double preloaded nut in cube case
+# Dimenzije vretena
+# Tip B+B+K Double preloaded nut in cube case
 
 d_0 = 40.0   # Ddijametar vretena [mm]
 P = 10.      # Korak [mm]
 i = 3.       # Broj navoja matice
 D_w = 6.35   # Dfijametar kugle [mm]
 
-C_o = 87800  #Nominalno statičko opterećenje [N]
-C_a = 36170  #Nominalno dinamičko opterećenje [N]
+C_o = 87800  # Nominalno statičko opterećenje [N]
+C_a = 36170  # Nominalno dinamičko opterećenje [N]
 
-k = 173      #Faktor krutosti N/micro_m^(3/2)
-R = 1009     #Aksijalna krutost N/micro_m
+k = 173  # Faktor krutosti N/micro_m^(3/2)
+R = 1009  # Aksijalna krutost N/micro_m
 
-d_r = 34.0   #Dijametar korijena navoja [mm]
-L_r = 825    #Radna dužina vretena [mm]
-L_uk = 1025 #Udaljenst između ležajeva vretena [mm]
-f_S = 3. 
+d_r = 34.0  # Dijametar korijena navoja [mm]
+L_r = 825  # Radna dužina vretena [mm]
+L_uk = 1025  # Udaljenst između ležajeva vretena [mm]
+f_S = 3.
 import math
 
-F_v = 0.1 * C_a #N
-f_v = 1.0       #Odabran prema 
-Q_kr = ((math.pi)**3 * 500 * d_0**4)/(f_v*L_uk**2) 
-F_a_max = Q_kr/3.
+F_v = 0.1 * C_a  # N
+f_v = 1.0  # Odabran prema
+Q_kr = ((math.pi)**3 * 500 * d_0**4) / (f_v * L_uk**2)
+F_a_max = Q_kr / 3.
 
-print('Q_kr =',Q_kr)
+print('Q_kr =', Q_kr)
 
-print('F_amax =',F_a_max)
+print('F_amax =', F_a_max)
 
-F_n = 4700        
+F_n = 4700
 n_n = 350
 
 
-L_1_2 = ((C_a * 1.25)/ F_n)**3 * 10**6
+L_1_2 = ((C_a * 1.25) / F_n)**3 * 10**6
 
 print('L_1_2 = ', L_1_2)
 """
 #Moment inercije
 I_r = (math.pi * (d_r)**2) / 64.
 
-#Modul elastičnosti 
+#Modul elastičnosti
 E = 210. # [GPa] = 1e-9 [N/mm**2]
 
 L = 825. #hod vretena [mm]
 
 lmbd = 2. # koeficijent rubnih uvjeta
 
-F = lmbd * ((4. * (math.pi)**2) * ((210000 ) * I_r ))/(L**2) 
+F = lmbd * ((4. * (math.pi)**2) * ((210000 ) * I_r ))/(L**2)
 
 P = 20. * (d_r**4/L**2) * 10**4
 
--- Scripts/mot_red_plt.py
+++ Scripts/mot_red_plt.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-#coding: utf-8
+# coding: utf-8
 """
 Created on Sat Mar 26 13:33:44 2016
 
@@ -13,11 +13,11 @@ Motor: Dunkermotor BG 44 SI
 """
 import numpy as np
 
-N_NOM = 2860  #rpm
-T_NOM = 5.7/100.0 #
-N_MIN = 100 #rpm
+N_NOM = 2860  # rpm
+T_NOM = 5.7 / 100.0
+N_MIN = 100  # rpm
 
-T_const = 4.8/100. #Nm/A
+T_const = 4.8 / 100.  # Nm/A
 
 Eta_reduktor = 0.8
 #i_r = np.array()
@@ -27,15 +27,15 @@ I_REDUKTOR = 25
 I_KOSARA = 3.2
 I_TOTAL = I_REDUKTOR * I_KOSARA
 for i in range(len(I_ARR)):
-    I_ARR[i] = I_ARR[i]*I_KOSARA
+    I_ARR[i] = I_ARR[i] * I_KOSARA
 N_KOS_Nom = N_NOM / I_TOTAL
 T_kos_Nom = T_NOM * I_TOTAL * Eta_reduktor
 
-N_KOS = np.linspace(0, 60 , 121)
+N_KOS = np.linspace(0, 60, 121)
 N_motor_arr = []
 for i in I_ARR:
-    N_motor_arr.append(N_KOS*i)
- 
+    N_motor_arr.append(N_KOS * i)
+
 N_motor = N_KOS * I_TOTAL
 N_m_1 = N_KOS * I_ARR[1]
 import matplotlib
@@ -46,13 +46,13 @@ fig.set_size_inches(18.5, 10.5, forward=True)
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-for item,num in enumerate(N_motor_arr):
+for item, num in enumerate(N_motor_arr):
     #print (num)
     plt.plot(N_KOS, N_motor_arr[item], label='$i = %i*3.2$' % I_RED[item])
-#for i in range(len(N_motor_arr)):
+# for i in range(len(N_motor_arr)):
 #    plt.plot(N_KOS, N_motor_arr[i], label='$i = %i*3.2$' % I_RED[i])
 
-plt.ylim(ymax = 7000, ymin = 0)
+plt.ylim(ymax=7000, ymin=0)
 plt.minorticks_on()
 plt.grid(b=True, which='major', color='k', linestyle='-')
 plt.grid(b=True, which='minor', color='r', linestyle='--')
